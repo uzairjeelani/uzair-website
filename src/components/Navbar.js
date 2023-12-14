9f36c938d451c84d20d58d2be65a57b5e49a17e3
Uzair Jeelani <uzairjeel@Uzairs-MacBook-Pro.local>
2023-12-14 18:12:16 -0500

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,68 @@
+import React, {useState} from "react"
+import { Link} from "react-router-dom"
+import { NavLink} from "react-router-dom"
+import "./Navbar.css"
+import {GiRocketThruster} from "react-icons/gi"
+import {FaBars, FaTimes} from "react-icons/fa"
+import {IconContext} from "react-icons/lib"
+
+function Navbar() {
+    const[click,setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    return (
+        <>
+        <IconContext.Provider value={{color: "#fff"}}>
+        <nav className="navbar">
+            <div className="navbar-container container">
+                <Link to="/" class="navbar-logo">
+                    Uzair Jeelani
+                </Link>
+                <div className="menu-icon" onClick={handleClick}>
+                {click ? <FaTimes /> : <FaBars />}
+            </div>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+                <NavLink 
+                to="/" 
+                className={({ isActive }) => 
+                "nav-links" + (isActive ? " activated" : "")
+                }
+                onClick={closeMobileMenu}
+                >
+                Home
+                </NavLink>
+            </li>
+            <li className="nav-item">
+                <NavLink 
+                to="/about" 
+                className={({ isActive }) => 
+                "nav-links" + (isActive ? " activated" : "")
+                }
+                onClick={closeMobileMenu}
+                >
+                About
+                </NavLink>
+            </li>
+            <li className="nav-item">
+                <NavLink 
+                to="/projects" 
+                className={({ isActive }) => 
+                "nav-links" + (isActive ? " activated" : "")
+                }
+                onClick={closeMobileMenu}
+                >
+                Projects
+                </NavLink>
+            </li>
+           </ul>
+          </div>
+        </nav>
+        </IconContext.Provider>
+        </>
+    );
+}
+
+export default Navbar;
