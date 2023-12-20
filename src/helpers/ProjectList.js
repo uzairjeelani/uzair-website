09235b27f4fc6df0efad0875c48ca75b83aaf955
Uzair <uzairjeelani@gmail.com>
2023-12-19 20:33:37 -0500

    change

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,7 +1,7 @@
 import TEST from "../assets/language.png";
 export const ProjectList = [
   {
-    name: "Power of Math Calcultor",
+    name: "Language Translator App",
     image: TEST,
     skills: "JavaScript,HTML,CSS",
     linkToPage: "http://translator-uzair.s3-website.us-east-2.amazonaws.com/",
