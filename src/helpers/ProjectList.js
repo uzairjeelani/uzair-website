28ca6e901db1ea9bf41e4b717b12e8c099ca53ac
Uzair <uzairjeelani@gmail.com>
2023-12-19 22:50:28 -0500

    change

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,7 +1,7 @@
 import TEST from "../assets/language.png";
 export const ProjectList = [
   {
-    name: "Language Translator App",
+    name: "Language Translator",
     image: TEST,
     skills: "JavaScript,HTML,CSS",
     linkToPage: "http://translator-uzair.s3-website.us-east-2.amazonaws.com/",
