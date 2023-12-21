6057225e1fe250f5f4d75208a6d08bf031846d5a
Uzair <uzairjeelani@gmail.com>
2023-12-20 23:35:40 -0500

    change

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,9 +1,16 @@
-import TEST from "../assets/language.png";
+import Project1 from "../assets/language.png";
+import Project2 from "../assets/store.png";
 export const ProjectList = [
   {
     name: "Language Translator",
-    image: TEST,
+    image: Project1,
     skills: "JavaScript,HTML,CSS",
     linkToPage: "http://translator-uzair.s3-website.us-east-2.amazonaws.com/",
+  },
+{
+    name: "E-Commerce Shoe Store",
+    image: Project2,
+    skills: "JavaScript,HTML,CSS",
+    linkToPage: "https://ecommerce-uzair.s3.us-east-2.amazonaws.com/ecommerce/index.html",
   },
 ];
