e23c299058677a8d2d17eea1be98723feba36a97
Uzair <uzairjeelani@gmail.com>
2023-12-21 21:31:50 -0500

    change

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,5 +1,6 @@
 import Project1 from "../assets/language.png";
 import Project2 from "../assets/store.png";
+import Project3 from "../assets/trivia.png";
 export const ProjectList = [
   {
     name: "Language Translator",
@@ -12,5 +13,11 @@ export const ProjectList = [
     image: Project2,
     skills: "JavaScript,HTML,CSS",
     linkToPage: "https://ecommerce-uzair.s3.us-east-2.amazonaws.com/ecommerce/index.html",
+  },
+{
+    name: "Niner Trivia Game",
+    image: Project3,
+    skills: "JavaScript,HTML,CSS",
+    linkToPage: "https://webpages.charlotte.edu/ujeelani/itis3135/lab9/index.html",
   },
 ];
